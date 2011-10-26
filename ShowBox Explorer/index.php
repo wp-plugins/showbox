1bb4c0f71cbd86e21c2518ae7dea71b66d0a11da
portablecomp <portablecomp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-26 18:38:49 +0000

    ShowBox 1.8 version: live help chat added.
    git-svn-id: https://plugins.svn.wordpress.org/showbox/trunk@456038 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ShowBox Explorer/index.php
+++ ShowBox Explorer/index.php
@@ -1,3 +1,6 @@
+<!-- ClickDesk - <a href='http://www.clickdesk.com'> Live Chat Service </a> for websites --><script type='text/javascript'>var _glc =_glc || [];_glc.push('ag9jb250YWN0dXN3aWRnZXRyDwsSB3dpZGdldHMY8LNNDA');var glcpath = (('https:' == document.location.protocol) ? 'https://contactuswidget.appspot.com/livily/browser/' : 'http://gae.clickdesk.com/livily/browser/');var glcp = (('https:' == document.location.protocol) ? 'https://' : 'http://');var glcspt = document.createElement('script'); glcspt.type = 'text/javascript'; glcspt.async = true;glcspt.src = glcpath + 'livechat.js';var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(glcspt, s);</script>
+
+
 <?php
 
 // Show login part...
